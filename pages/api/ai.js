bbd4eb742b4b79c3b2f309317a5f432e3a51b269
Munavvar sinan k p <munavvarsinan01@gmail.com>
2022-12-03 23:18:27 +0530

    minor changes

--- pages/api/ai.js
+++ pages/api/ai.js
@@ -1,7 +1,7 @@
 const { Configuration, OpenAIApi } = require('openai');
 
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: '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',
 });
 
 const openai = new OpenAIApi(configuration);
